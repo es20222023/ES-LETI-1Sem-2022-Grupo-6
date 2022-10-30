626eaa7dc35f4880634522d3303934a0941f3003
7jtom <7jtom@DESKTOP-4Q3066M.lan>
2022-10-30 23:01:15 +0000

    Coisas básicas

---
+++ Projeto ES/src/app/FileHandler.java
@@ -0,0 +1,53 @@
+package app;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class FileHandler {
+
+	/**
+	 * Funcao que le textfile e retorna uma arraylist em que cada elemento é um dos
+	 * eventos do calendario em texto
+	 * 
+	 * @param path
+	 * @return calendar
+	 * @throws FileNotFoundException
+	 */
+
+	public static ArrayList<String> readTextFile(String path) throws FileNotFoundException {
+
+		File text = new File(path);
+		Scanner scanner = new Scanner(text);
+
+		ArrayList<String> calendar = new ArrayList<String>();
+		String calendarEvent = "";
+
+		while (scanner.hasNextLine()) {
+			String line = scanner.nextLine();
+
+			calendarEvent += line + "\n";
+			
+			if (line.equals("BEGIN:VEVENT"))
+				calendarEvent = "";
+
+			else if (line.equals("END:VEVENT"))
+				calendar.add(calendarEvent);
+		}
+		return calendar;
+	}
+
+	public static void main(String[] args) {
+		String path = "C:\\Users\\7jtom\\git\\ES-LETI-1Sem-2022-Grupo-6\\Projeto ES\\files\\thgas";
+		ArrayList<String> teste = new ArrayList<String>();
+		try {
+			teste = readTextFile(path);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println(teste.get(0));
+	}
+
+}
