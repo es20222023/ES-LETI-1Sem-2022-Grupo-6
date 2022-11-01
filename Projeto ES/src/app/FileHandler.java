52afef26b5173d3ce2f47ccd3bb01b62a3c2a757
7jtom <7jtom@DESKTOP-4Q3066M.lan>
2022-11-01 18:20:45 +0000

    Classe FileHandler

--- Projeto ES/src/app/FileHandler.java
+++ Projeto ES/src/app/FileHandler.java
@@ -9,49 +9,68 @@ public class FileHandler {
 
 	/**
 	 * Funcao que le textfile e retorna uma arraylist em que cada elemento é um dos
-	 * eventos do calendario em texto
+	 * eventos do calendario
 	 * 
 	 * @param path - caminho do ficheiro que se quer ler
 	 * @return calendar
 	 * @throws FileNotFoundException
 	 */
 
-	public static ArrayList<String> readTextFile(String path) throws FileNotFoundException {
+	public static ArrayList<CalendarEvent> readTextFile(String path) throws FileNotFoundException {
 
 		File text = new File(path);
 		Scanner scanner = new Scanner(text);
 
-		ArrayList<String> calendar = new ArrayList<String>();
+		ArrayList<CalendarEvent> calendarEvents = new ArrayList<CalendarEvent>();
 		String calendarEvent = "";
 
 		while (scanner.hasNextLine()) {
 			String line = scanner.nextLine();
 
 //			calendarEvent += line + "\n";
-			
+
 			if (line.equals("BEGIN:VEVENT"))
 				calendarEvent = "";
 
 			else if (line.equals("END:VEVENT"))
-				calendar.add(calendarEvent);
+				calendarEvents.add(convertStringToCalendarEvent(calendarEvent));
 			else
 				calendarEvent += line + "\n";
 		}
-		return calendar;
+		return calendarEvents;
 	}
 
-	
+	/**
+	 * Função que converte a String com os dados respetivos em CalendarEvent
+	 * 
+	 * @param event
+	 * @return
+	 */
 
-	public static void main(String[] args) {
-		String path = "C:\\Users\\7jtom\\git\\ES-LETI-1Sem-2022-Grupo-6\\Projeto ES\\files\\thgas";
-		ArrayList<String> teste = new ArrayList<String>();
-		try {
-			teste = readTextFile(path);
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		System.out.println(teste.get(0));
+	private static CalendarEvent convertStringToCalendarEvent(String event) {
+
+		String dateStart = getSubString(event, "DTSTART:", "DTEND:");
+		String dateEnd = getSubString(event, "DTEND:", "SUMMARY:");
+		String summary = getSubString(event, "SUMMARY:", "UID:");
+		String description = getSubString(event, "DESCRIPTION:", "LOCATION:");
+		String location = getSubString(event, "LOCATION:", "");
+
+		return (new CalendarEvent(dateStart, dateEnd, summary, description, location));
+
 	}
 
+	/**
+	 * Funcao que retorna a substring entre firstSubString e secondSubString
+	 * 
+	 * @param totalString
+	 * @param firstSubString
+	 * @param secondSubString
+	 * @return
+	 */
+	private static String getSubString(String totalString, String firstSubString, String secondSubString) {
+		int start = totalString.indexOf(firstSubString) + firstSubString.length();
+		int end = totalString.lastIndexOf(secondSubString);
+
+		return totalString.substring(start, end);
+	}
 }
