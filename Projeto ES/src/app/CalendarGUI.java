bdf46b828b08dc7141dbe1d7b85e3a0af7356377
thgas-iscte <thgas@iscte-iul.pt>
2022-11-21 01:49:25 +0000

    Utilizar escolher as datas da pesquisa

--- Projeto ES/src/app/CalendarGUI.java
+++ Projeto ES/src/app/CalendarGUI.java
@@ -10,9 +10,10 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Writer;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.time.Instant;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.HashMap;
 
 import javax.swing.JButton;
@@ -28,18 +29,15 @@ import com.github.mustachejava.MustacheFactory;
 
 public class CalendarGUI {
 
+	private static final String HTML_FILES_PATH = "files/html_files/";
 	private JFrame frame;
 
 	public CalendarGUI() {
-
 		frame = new JFrame();
-		start();
-
 	}
 
-	private void start() {
+	public void start() {
 		askUserForNameAndURL();
-		// TODO a parte da GUI a aparecer o calendário
 	}
 
 	private void askUserForNameAndURL() {
@@ -52,7 +50,7 @@ public class CalendarGUI {
 	}
 
 	private void addUserInputFrameContent(JFrame userInputFrame) {
-		userInputFrame.setLayout(new GridLayout(3, 3));
+		userInputFrame.setLayout(new GridLayout(5, 3));
 
 		JLabel askNameLabel = new JLabel("Introduza o seu nome", SwingConstants.CENTER);
 		JTextField askName = new JTextField();
@@ -60,54 +58,89 @@ public class CalendarGUI {
 		JLabel askURLLabel = new JLabel("Introduza o seu URL", SwingConstants.CENTER);
 		JTextField askURL = new JTextField();
 
-		JLabel buttonLabel = new JLabel("Carregue neste botão para carregar o seu calendário", SwingConstants.CENTER);
-		JButton button = new JButton("Carregar calendário");
+		JLabel askDateStartLabel = new JLabel("Introduza a data de início da pesquisa em formato yyyy-MM-dd-HH-mm",
+				SwingConstants.CENTER);
+		JTextField askDateStart = new JTextField();
+
+		JLabel askDateEndLabel = new JLabel("Introduza a data de fim da pesquisa em formato yyyy-MM-dd-HH-mm",
+				SwingConstants.CENTER);
+		JTextField askDateEnd = new JTextField();
+
+		JLabel buttonLabel = new JLabel("Carregue neste botão para abrir o calendário", SwingConstants.CENTER);
+		JButton button = new JButton("Abrir calendário");
 
 		userInputFrame.add(askNameLabel);
 		userInputFrame.add(askName);
 		userInputFrame.add(askURLLabel);
 		userInputFrame.add(askURL);
+		userInputFrame.add(askDateStartLabel);
+		userInputFrame.add(askDateStart);
+		userInputFrame.add(askDateEndLabel);
+		userInputFrame.add(askDateEnd);
 		userInputFrame.add(buttonLabel);
 		userInputFrame.add(button);
 
 		button.addActionListener(new ActionListener() {
-
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				String name = askName.getText();
 				String URL = askURL.getText();
+				String dateStart = askDateStart.getText();
+				String dateEnd = askDateEnd.getText();
+
+				SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd-HH-mm");
 
 				userInputFrame.setVisible(false);
 				userInputFrame.dispose();
 
-				UrlReader.Urlcatcher(URL, name);
+				File txtFileDir = new File("files/text_files/" + name + ".txt");
+				if (!txtFileDir.exists()) {
+					UrlReader.Urlcatcher(URL, name);
+					FileHandler.createNewCalendarFile(name);
+				}
+				ArrayList<CalendarEvent> events = FileHandler.decodeJSONFile(name);
+				try {
+					if (dateStart.equals("") && dateEnd.equals(""))
+						createHTMLTable(events, null, null, name);
+					else
+						createHTMLTable(events, format.parse(dateStart).toInstant(), format.parse(dateEnd).toInstant(),
+								name);
+				} catch (IOException | ParseException e1) {
+					e1.printStackTrace();
+				}
 			}
 		});
 	}
 
 	private FileReader getTemplate() throws FileNotFoundException {
-		return new FileReader(new File("files/html_files/template.html"));
+		return new FileReader(new File(HTML_FILES_PATH + "template.html"));
 	}
 
 	public void createHTMLTable(ArrayList<CalendarEvent> calendarEvents, Instant dateStart, Instant dateEnd,
-			String path) throws IOException {
+			String fileName) throws IOException {
 		ArrayList<CalendarEvent> events = getEventsBetweenDates(calendarEvents, dateStart, dateEnd);
 
 		HashMap<String, Object> scopes = new HashMap<String, Object>();
 		scopes.put("events", events);
 
-		Writer writer = new FileWriter(path);
+		Writer writer = new FileWriter(HTML_FILES_PATH + fileName + ".html");
 		MustacheFactory mf = new DefaultMustacheFactory();
 		Mustache mustache = mf.compile(getTemplate(), "magicName");
 		mustache.execute(writer, scopes);
 		writer.flush();
 
-		File htmlFile = new File(path);
+		openHTMLFile(fileName);
+	}
+
+	private void openHTMLFile(String fileName) throws IOException {
+		File htmlFile = new File(HTML_FILES_PATH + fileName);
 		Desktop.getDesktop().browse(htmlFile.toURI());
 	}
 
 	private ArrayList<CalendarEvent> getEventsBetweenDates(ArrayList<CalendarEvent> calendarEvents, Instant dateStart,
 			Instant dateEnd) {
+		if (dateEnd == null || dateStart == null)
+			return calendarEvents;
 		ArrayList<CalendarEvent> events = new ArrayList<>();
 		for (CalendarEvent c : calendarEvents)
 			if (c.getDateStart().isAfter(dateStart) && c.getDateStart().isBefore(dateEnd)
@@ -116,20 +149,8 @@ public class CalendarGUI {
 		return events;
 	}
 
-	@SuppressWarnings("deprecation")
 	public static void main(String[] args) {
 		CalendarGUI c = new CalendarGUI();
-		Calendar cccc = new Calendar("thgas.json");
-		ArrayList<CalendarEvent> cal = cccc.getCalendarEvents();
-
-		Date d1 = new Date(2022 - 1900, 10, 21, 13, 0);
-		Date d2 = new Date(2022 - 1900, 10, 21, 20, 00);
-
-		try {
-			c.createHTMLTable(cal, d1.toInstant(), d2.toInstant(), "files/html_files/teste.html");
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		c.start();
 	}
 }
