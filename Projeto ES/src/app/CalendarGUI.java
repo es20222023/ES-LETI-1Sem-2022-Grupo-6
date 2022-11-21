e4058e0248aa486a4162e635faa5a5ae5bbcc1a5
thgas-iscte <thgas@iscte-iul.pt>
2022-11-21 00:26:41 +0000

    Filtrar dados entre duas datas

--- Projeto ES/src/app/CalendarGUI.java
+++ Projeto ES/src/app/CalendarGUI.java
@@ -12,6 +12,7 @@ import java.io.IOException;
 import java.io.Writer;
 import java.time.Instant;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 
 import javax.swing.JButton;
@@ -90,13 +91,14 @@ public class CalendarGUI {
 
 	public void createHTMLTable(ArrayList<CalendarEvent> calendarEvents, Instant dateStart, Instant dateEnd,
 			String path) throws IOException {
+		ArrayList<CalendarEvent> events = getEventsBetweenDates(calendarEvents, dateStart, dateEnd);
 
 		HashMap<String, Object> scopes = new HashMap<String, Object>();
-		scopes.put("calendarEvents", calendarEvents);
+		scopes.put("events", events);
 
 		Writer writer = new FileWriter(path);
 		MustacheFactory mf = new DefaultMustacheFactory();
-		Mustache mustache = mf.compile(getTemplate(), "example");
+		Mustache mustache = mf.compile(getTemplate(), "magicName");
 		mustache.execute(writer, scopes);
 		writer.flush();
 
@@ -104,12 +106,27 @@ public class CalendarGUI {
 		Desktop.getDesktop().browse(htmlFile.toURI());
 	}
 
+	private ArrayList<CalendarEvent> getEventsBetweenDates(ArrayList<CalendarEvent> calendarEvents, Instant dateStart,
+			Instant dateEnd) {
+		ArrayList<CalendarEvent> events = new ArrayList<>();
+		for (CalendarEvent c : calendarEvents)
+			if (c.getDateStart().isAfter(dateStart) && c.getDateStart().isBefore(dateEnd)
+					|| c.getDateStart().equals(dateStart) || c.getDateStart().equals(dateEnd))
+				events.add(c);
+		return events;
+	}
+
+	@SuppressWarnings("deprecation")
 	public static void main(String[] args) {
 		CalendarGUI c = new CalendarGUI();
 		Calendar cccc = new Calendar("thgas.json");
 		ArrayList<CalendarEvent> cal = cccc.getCalendarEvents();
+
+		Date d1 = new Date(2022 - 1900, 10, 21, 13, 0);
+		Date d2 = new Date(2022 - 1900, 10, 21, 20, 00);
+
 		try {
-			c.createHTMLTable(cal, null, null, "files/html_files/teste.html");
+			c.createHTMLTable(cal, d1.toInstant(), d2.toInstant(), "files/html_files/teste.html");
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
